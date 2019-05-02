96bb99364ec89b328dd5dff5e528168ddc6f7fbf
Rene Calunsag Jr <rcalunsag@ewise.com>
2019-05-03 02:14:39 +0800

    Renaming tests so it doesn't look weird

--- src/__tests__/1.test.js
+++ src/__tests__/1.test.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { View } from 'react-native'
 
-describe('1', () => {
-  it('2', () => {
+describe('identity', () => {
+  it('1', () => {
     expect(1).toEqual(1)
   })
 })
